8d0c9297b84487ac7d0ba4349b0f6ae76cc8269a
Greg Bergé <berge.greg@gmail.com>
2022-08-08 14:19:11 +0200

    chore: use npm

--- packages/argos-cli/dev/push_with_global.sh
+++ packages/argos-cli/dev/push_with_global.sh
@@ -3,4 +3,4 @@ set -ev
 
 export ARGOS_COMMIT=e5ff79c2137ed3dd321d0e9a2ceba98bba11f579
 export ARGOS_BRANCH=test
-yarn argos upload packages/argos-cli/__fixtures__
+npx argos upload packages/argos-cli/__fixtures__
