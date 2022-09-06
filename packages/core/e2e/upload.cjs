2d348263f395ed0170fa57da289c3f8fa39f755e
Greg Bergé <berge.greg@gmail.com>
2022-09-06 13:39:26 +0200

    chore: run tests on Node.js 14 & 16

--- packages/core/e2e/upload.cjs
+++ packages/core/e2e/upload.cjs
@@ -1,4 +1,4 @@
 const { upload } = require("@argos-ci/core");
 const { runUpload } = require("./run-upload.cjs");
 
-runUpload(upload, "argos-core-cjs-e2e");
+runUpload(upload, `argos-core-e2e-cjs-node-v${process.env.NODE_VERSION}`);
