91aacd96234059694e5174ca9cdeb4b2483d317b
Greg Bergé <berge.greg@gmail.com>
2022-10-15 14:34:45 +0200

    chore(ci): test on windows

--- packages/core/e2e/upload.cjs
+++ packages/core/e2e/upload.cjs
@@ -1,4 +1,7 @@
 const { upload } = require("@argos-ci/core");
 const { runUpload } = require("./run-upload.cjs");
 
-runUpload(upload, `argos-core-e2e-cjs-node-v${process.env.NODE_VERSION}`);
+runUpload(
+  upload,
+  `argos-core-e2e-cjs-node-${process.env.NODE_VERSION}-${process.env.OS}`
+);
--- packages/core/e2e/upload.mjs
+++ packages/core/e2e/upload.mjs
@@ -1,4 +1,7 @@
 import { upload } from "@argos-ci/core";
 import { runUpload } from "./run-upload.cjs";
 
-runUpload(upload, `argos-core-e2e-mjs-node-v${process.env.NODE_VERSION}`);
+runUpload(
+  upload,
+  `argos-core-e2e-mjs-node-${process.env.NODE_VERSION}-${process.env.OS}`
+);
