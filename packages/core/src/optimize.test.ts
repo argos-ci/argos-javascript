169491eefdfe9aaefa94df2ee164dee1565be10d
Greg Bergé <berge.greg@gmail.com>
2023-09-25 22:32:21 +0200

    chore: fix import

--- packages/core/src/optimize.test.ts
+++ packages/core/src/optimize.test.ts
@@ -2,7 +2,6 @@ import { describe, it, expect } from "vitest";
 import { join } from "node:path";
 // import { fileURLToPath } from "node:url";
 import { stat } from "node:fs/promises";
-import { optimizeScreenshot } from "./optimize";
 
 // const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
@@ -16,10 +15,11 @@ const exists = async (filepath: string) => {
 };
 
 // This test does not run on CI, a sharp install issue..
-describe.skip("#optimizeScreenshot", () => {
+describe("#optimizeScreenshot", () => {
   it("optimizes", async () => {
+    const { optimizeScreenshot } = await import("./optimize");
     const optimizedPath = await optimizeScreenshot(
-      join(__dirname, "../../../__fixtures__/screenshots/penelope.jpg"),
+      join(__dirname, "../../../__fixtures__/screenshots/penelope.jpg")
     );
     expect(await exists(optimizedPath)).toBe(true);
   });
