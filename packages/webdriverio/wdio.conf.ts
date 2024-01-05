57765715e065a2464695355fc6778a3892ba6470
Greg Bergé <berge.greg@gmail.com>
2024-01-05 12:14:16 +0100

    chore: run chrome headless

--- packages/webdriverio/wdio.conf.ts
+++ packages/webdriverio/wdio.conf.ts
@@ -58,7 +58,13 @@ export const config: Options.Testrunner = {
   //
   capabilities: [
     {
+      maxInstances: 5,
       browserName: "chrome",
+      acceptInsecureCerts: true,
+      // We need to extends some Chrome flags in order to tell Chrome to run headless
+      "goog:chromeOptions": {
+        args: ["--headless", "--disable-gpu", "--disable-dev-shm-usage"],
+      },
     },
   ],
 
