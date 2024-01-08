700eeb884ea5841d9a63bb511feced06e1d643cf
Jeremy Sfez <jeremy@smooth-code.com>
2024-01-08 16:53:07 +0100

    feat: improve sharp error message

--- packages/core/src/optimize.ts
+++ packages/core/src/optimize.ts
@@ -7,13 +7,20 @@ export type ImageFormat = keyof sharp.FormatEnum;
 const tmpFile = promisify<string>(tmp.file);
 
 export const optimizeScreenshot = async (filepath: string): Promise<string> => {
-  const resultFilePath = await tmpFile();
-  await sharp(filepath)
-    .resize(2048, 20480, {
-      fit: "inside",
-      withoutEnlargement: true,
-    })
-    .png({ force: true })
-    .toFile(resultFilePath);
-  return resultFilePath;
+  try {
+    const resultFilePath = await tmpFile();
+    await sharp(filepath)
+      .resize(2048, 20480, {
+        fit: "inside",
+        withoutEnlargement: true,
+      })
+      .png({ force: true })
+      .toFile(resultFilePath);
+    return resultFilePath;
+  } catch (error) {
+    const message = error instanceof Error ? error.message : "Unknown Error";
+    throw new Error(`Error while processing image (${filepath}): ${message}`, {
+      cause: error,
+    });
+  }
 };
