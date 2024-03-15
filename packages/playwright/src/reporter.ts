507a833f5896421f24fa83a6b1338ca565b06da8
Greg Bergé <berge.greg@gmail.com>
2024-03-15 12:58:31 +0100

    fix(playwright): fix upload dir creation

--- packages/playwright/src/reporter.ts
+++ packages/playwright/src/reporter.ts
@@ -22,10 +22,12 @@ import {
 import { getMetadataFromTestCase } from "./metadata";
 import { debug } from "./debug";
 
-async function createTempDirectory() {
+async function createUploadDirectory() {
+  debug("Creating temporary directory for screenshots");
   const osTmpDirectory = tmpdir();
   const path = join(osTmpDirectory, "argos." + randomBytes(16).toString("hex"));
   await mkdir(path, { recursive: true });
+  debug(`Temporary directory created: ${path}`);
   return path;
 }
 
@@ -55,7 +57,7 @@ async function getParallelFromConfig(
 }
 
 class ArgosReporter implements Reporter {
-  uploadDir!: string;
+  createUploadDirPromise: null | Promise<string>;
   config: ArgosReporterOptions;
   playwrightConfig!: FullConfig;
   uploadToArgos: boolean;
@@ -63,11 +65,13 @@ class ArgosReporter implements Reporter {
   constructor(config: ArgosReporterOptions) {
     this.config = config;
     this.uploadToArgos = config.uploadToArgos ?? true;
+    this.createUploadDirPromise = null;
   }
 
   async writeFile(path: string, body: Buffer | string) {
+    const uploadDir = await this.getUploadDir();
     const dir = dirname(path);
-    if (dir !== this.uploadDir) {
+    if (dir !== uploadDir) {
       await mkdir(dir, { recursive: true });
     }
     debug(`Writing file to ${path}`);
@@ -76,8 +80,9 @@ class ArgosReporter implements Reporter {
   }
 
   async copyFile(from: string, to: string) {
+    const uploadDir = await this.getUploadDir();
     const dir = dirname(to);
-    if (dir !== this.uploadDir) {
+    if (dir !== uploadDir) {
       await mkdir(dir, { recursive: true });
     }
     debug(`Copying file from ${from} to ${to}`);
@@ -95,15 +100,20 @@ class ArgosReporter implements Reporter {
     return name;
   }
 
-  async onBegin(config: FullConfig, _suite: Suite) {
+  getUploadDir() {
+    if (!this.createUploadDirPromise) {
+      this.createUploadDirPromise = createUploadDirectory();
+    }
+    return this.createUploadDirPromise;
+  }
+
+  onBegin(config: FullConfig, _suite: Suite) {
     debug("ArgosReporter:onBegin");
     this.playwrightConfig = config;
-    debug(`Creating temporary directory for uploads`);
-    this.uploadDir = await createTempDirectory();
-    debug(`Temporary directory created for uploads: ${this.uploadDir}`);
   }
 
   async onTestEnd(test: TestCase, result: TestResult) {
+    const uploadDir = await this.getUploadDir();
     debug("ArgosReporter:onTestEnd");
     await Promise.all(
       result.attachments.map(async (attachment) => {
@@ -111,10 +121,7 @@ class ArgosReporter implements Reporter {
           checkIsArgosScreenshot(attachment) ||
           checkIsArgosScreenshotMetadata(attachment)
         ) {
-          const path = join(
-            this.uploadDir,
-            getAttachmentFilename(attachment.name),
-          );
+          const path = join(uploadDir, getAttachmentFilename(attachment.name));
           await this.copyFile(attachment.path, path);
           return;
         }
@@ -124,7 +131,7 @@ class ArgosReporter implements Reporter {
           const trace = result.attachments.find(checkIsTrace) ?? null;
           const metadata = await getMetadataFromTestCase(test, result);
           const name = this.getAutomaticScreenshotName(test, result);
-          const path = join(this.uploadDir, `${name}.png`);
+          const path = join(uploadDir, `${name}.png`);
           await Promise.all([
             this.writeFile(path + ".argos.json", JSON.stringify(metadata)),
             this.copyFile(attachment.path, path),
@@ -138,9 +145,10 @@ class ArgosReporter implements Reporter {
 
   async onEnd(_result: FullResult) {
     debug("ArgosReporter:onEnd");
+    const uploadDir = await this.getUploadDir();
     if (!this.uploadToArgos) {
       debug("Not uploading to Argos because uploadToArgos is false.");
-      debug(`Upload directory: ${this.uploadDir}`);
+      debug(`Upload directory: ${uploadDir}`);
       return;
     }
 
@@ -158,7 +166,7 @@ class ArgosReporter implements Reporter {
       debug("Uploading to Argos");
       const res = await upload({
         files: ["**/*.png"],
-        root: this.uploadDir,
+        root: uploadDir,
         parallel: parallel ?? undefined,
         ...this.config,
       });
