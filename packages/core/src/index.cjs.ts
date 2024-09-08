10f67bc78d03b4537053c5210992d487e6ed2f1e
Greg Bergé <berge.greg@gmail.com>
2024-09-08 21:19:12 +0200

    fix: fix common js finalize & readConfig

--- packages/core/src/index.cjs.ts
+++ packages/core/src/index.cjs.ts
@@ -1,7 +1,17 @@
-import type { UploadParameters } from "./index";
-
-exports.upload = async (params: UploadParameters) => {
+exports.upload = async (...args: any[]) => {
   // @ts-ignore
   const { upload } = await import("./index.mjs");
-  return upload(params);
+  return upload(...args);
+};
+
+exports.finalize = async (...args: any[]) => {
+  // @ts-ignore
+  const { finalize } = await import("./index.mjs");
+  return finalize(...args);
+};
+
+exports.readConfig = async (...args: any[]) => {
+  // @ts-ignore
+  const { readConfig } = await import("./index.mjs");
+  return readConfig(...args);
 };
