70a376cba75cb073c8199e4c96992fc2f37e9a7f
Greg Bergé <berge.greg@gmail.com>
2024-10-27 15:00:11 +0100

    docs: document stabilization options

--- packages/cypress/src/support.ts
+++ packages/cypress/src/support.ts
@@ -36,10 +36,9 @@ type ArgosScreenshotOptions = Partial<
   threshold?: number;
 
   /**
-   * Stabilization.
-   * By default, it waits for the UI to stabilize before taking a screenshot.
+   * Wait for the UI to stabilize before taking the screenshot.
    * Set to `false` to disable stabilization.
-   * Pass an object to customize the stabilization process.
+   * Pass an object to customize the stabilization.
    * @default true
    */
   stabilize?: boolean | StabilizationOptions;
