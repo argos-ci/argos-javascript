c0048a3412d84aa70d89bee40d0b32a602ffe32b
Greg Bergé <berge.greg@gmail.com>
2025-01-14 09:45:45 +0100

    chore: log from cli in ci

--- packages/cli/e2e/upload.js
+++ packages/cli/e2e/upload.js
@@ -1,11 +1,14 @@
-import { exec } from "child_process";
+import { exec } from "node:child_process";
 
 exec(
   `node bin/argos-cli.js upload ../../__fixtures__ --build-name "argos-cli-e2e-node-${process.env.NODE_VERSION}-${process.env.OS}"`,
-  (err) => {
+  (err, stdout, stderr) => {
     if (err) {
       console.error(err);
       process.exit(1);
     }
+
+    console.log(stdout);
+    console.error(stderr);
   },
 );
