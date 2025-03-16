162ee642131e0563a74e5b6e79b441279ba9793c
Greg Bergé <berge.greg@gmail.com>
2025-03-16 08:07:49 +0100

    docs: clarify the Storybook play function behaviour

---
+++ packages/storybook/stories/Form.stories.js
@@ -0,0 +1,34 @@
+import { userEvent, within } from "@storybook/test";
+
+import { Form } from "./Form";
+
+export default {
+  title: "Example/Form",
+  component: Form,
+};
+
+export const FillForm = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const emailInput = canvas.getByLabelText("Email", {
+      selector: "input",
+    });
+
+    await userEvent.type(emailInput, "example-email@email.com", {
+      delay: 100,
+    });
+
+    const passwordInput = canvas.getByLabelText("Password", {
+      selector: "input",
+    });
+
+    await userEvent.type(passwordInput, "ExamplePassword", {
+      delay: 100,
+    });
+    // See https://storybook.js.org/docs/essentials/actions#automatically-matching-args to learn how to setup logging in the Actions panel
+    const submitButton = canvas.getByRole("button");
+
+    await userEvent.click(submitButton);
+  },
+};
