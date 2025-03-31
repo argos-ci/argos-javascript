f2dbcbc1cb90542716ec7accf814bf040519abad
Greg Bergé <berge.greg@gmail.com>
2025-03-31 21:20:26 +0200

    feat(storybook): support argos.modes
    Similar to chromatic.modes, it allows to capture screenshots
    on different viewports and themes and any global used in any plugin.

--- packages/storybook/.storybook/preview.ts
+++ packages/storybook/.storybook/preview.ts
@@ -1,12 +1,34 @@
 import type { Preview } from "@storybook/react";
 
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { withThemeByClassName } from "@storybook/addon-themes";
+import { allModes } from "./modes";
+
 const preview: Preview = {
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+  ],
   parameters: {
+    argos: {
+      modes: {
+        light: allModes["light"],
+        dark: allModes["dark"],
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
     },
   },
 };
