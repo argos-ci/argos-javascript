a385a2c18ffc51c4284038e90876b71036063055
Greg Bergé <berge.greg@gmail.com>
2025-05-15 14:58:36 +0200

    feat: add debug info for merge base

--- packages/core/src/debug.ts
+++ packages/core/src/debug.ts
@@ -4,16 +4,16 @@ const KEY = "@argos-ci/core";
 
 export const debug = createDebug(KEY);
 
+export const isDebugEnabled = createDebug.enabled(KEY);
+
 export const debugTime = (arg: string) => {
-  const enabled = createDebug.enabled(KEY);
-  if (enabled) {
+  if (isDebugEnabled) {
     console.time(arg);
   }
 };
 
 export const debugTimeEnd = (arg: string) => {
-  const enabled = createDebug.enabled(KEY);
-  if (enabled) {
+  if (isDebugEnabled) {
     console.timeEnd(arg);
   }
 };
