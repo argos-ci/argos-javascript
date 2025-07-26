cca2f372c2f3775ac4dd3cc4b2759cf4dc7cc9fe
Greg Bergé <berge.greg@gmail.com>
2025-07-26 13:48:27 +0200

    chore: upgrade to Storybook v9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import eslint from "@eslint/js";
 import vitest from "@vitest/eslint-plugin";
 import tseslint from "typescript-eslint";
