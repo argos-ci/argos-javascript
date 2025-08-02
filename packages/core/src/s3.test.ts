dee8fcb274cc317cd914c02936fb39633a4387aa
Greg Bergé <berge.greg@gmail.com>
2025-08-02 22:26:23 +0200

    feat: remove dependency to axios

--- packages/core/src/s3.test.ts
+++ packages/core/src/s3.test.ts
@@ -3,7 +3,7 @@ import { describe, it } from "vitest";
 import { join } from "node:path";
 // import { fileURLToPath } from "node:url";
 import { setupMockServer } from "../mocks/server";
-import { upload } from "./s3";
+import { uploadFile } from "./s3";
 
 // const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
@@ -11,7 +11,7 @@ setupMockServer();
 
 describe("#upload", () => {
   it("uploads", async () => {
-    await upload({
+    await uploadFile({
       path: join(__dirname, "../../../__fixtures__/screenshots/penelope.png"),
       url: "https://api.s3.dev/upload/123",
       contentType: "image/png",
@@ -19,7 +19,7 @@ describe("#upload", () => {
   });
 
   it("uploads big images", async () => {
-    await upload({
+    await uploadFile({
       path: join(__dirname, "../../../__fixtures__/png-10mb.png"),
       url: "https://api.s3.dev/upload/123",
       contentType: "image/png",
