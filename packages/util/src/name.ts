1543c916235a6c2269b67d9a995ba4fb307ce775
Greg Bergé <berge.greg@gmail.com>
2025-09-27 21:52:14 +0200

    fix(windows): sanitize screenshot path to be safe

--- packages/util/src/name.ts
+++ packages/util/src/name.ts
@@ -7,9 +7,42 @@ export function getScreenshotName(
     viewportWidth?: number;
   } = {},
 ) {
-  const parts = [name];
-  if (options.viewportWidth) {
-    parts.push(`vw-${options.viewportWidth}`);
+  return sanitizePath(
+    `${name}${options.viewportWidth ? ` vw-${options.viewportWidth}` : ""}`,
+  );
+}
+
+/**
+ * Sanitize a path to be safe on all OSes.
+ */
+function sanitizePath(name: string) {
+  return name
+    .split("/")
+    .map((filename) => sanitizeFilename(filename))
+    .join("/");
+}
+
+// From https://github.com/sindresorhus/filename-reserved-regex
+function filenameReservedRegex() {
+  // eslint-disable-next-line no-control-regex
+  return /[<>:"/\\|?*\u0000-\u001F]|[. ]$/g;
+}
+
+// From https://github.com/sindresorhus/filename-reserved-regex
+function windowsReservedNameRegex() {
+  return /^(con|prn|aux|nul|com\d|lpt\d)$/i;
+}
+
+/**
+ * Sanitize a filename to be safe on all OSes.
+ */
+function sanitizeFilename(name: string): string {
+  const replacement = "-";
+  const sanitized = name.replace(filenameReservedRegex(), replacement);
+
+  if (windowsReservedNameRegex().test(sanitized)) {
+    return `${sanitized}${replacement}`;
   }
-  return parts.join(" ");
+
+  return sanitized;
 }
