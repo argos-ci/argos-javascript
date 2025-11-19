a62b95a0b34b5dee9f2a8e5654053d2aaff7ed06
Greg Bergé <berge.greg@gmail.com>
2025-11-19 09:54:09 +0100

    fix(playwright): fix missing exports from `@argos-ci/playwright` in CJS
    Fix #250

--- packages/playwright/src/index.cjs
+++ packages/playwright/src/index.cjs
@@ -1,5 +1,26 @@
-const argosScreenshot = async (...args) => {
+async function argosAriaSnapshot(...args) {
+  const { argosAriaSnapshot } = await import("./index.js");
+  return argosAriaSnapshot(...args);
+}
+
+async function argosScreenshot(...args) {
   const { argosScreenshot } = await import("./index.js");
   return argosScreenshot(...args);
-};
+}
+
+function getCSPScriptHash(...args) {
+  // Loading ESM using require works in Node.js v22.13.0+
+  const { getCSPScriptHash } = require("./index.js");
+  return getCSPScriptHash(...args);
+}
+
+function DO_NOT_USE_setMetadataConfig(...args) {
+  // Loading ESM using require works in Node.js v22.13.0+
+  const { DO_NOT_USE_setMetadataConfig } = require("./index.js");
+  return DO_NOT_USE_setMetadataConfig(...args);
+}
+
+exports.argosAriaSnapshot = argosAriaSnapshot;
 exports.argosScreenshot = argosScreenshot;
+exports.getCSPScriptHash = getCSPScriptHash;
+exports.DO_NOT_USE_setMetadataConfig = DO_NOT_USE_setMetadataConfig;
