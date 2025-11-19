bc0939e40b0eaf52b48e6556333f77facbe92819
Greg Bergé <berge.greg@gmail.com>
2025-11-19 09:54:09 +0100

    fix(playwright): fix missing exports from `@argos-ci/playwright` in CJS
    Fix #250

--- packages/playwright/src/index.ts
+++ packages/playwright/src/index.ts
@@ -1,6 +1,11 @@
-export * from "./aria-snapshot";
-export * from "./screenshot";
-export * from "./csp";
+// Methods
+export { argosAriaSnapshot } from "./aria-snapshot";
+export { argosScreenshot } from "./screenshot";
+export { getCSPScriptHash } from "./csp";
 export { setMetadataConfig as DO_NOT_USE_setMetadataConfig } from "./metadata";
+
+// Types
+export type { ArgosSnapshotOptions } from "./aria-snapshot";
+export type { ArgosScreenshotOptions } from "./screenshot";
 export type { MetadataConfig } from "./metadata";
-export { type ArgosAttachment } from "./attachment";
+export type { ArgosAttachment } from "./attachment";
